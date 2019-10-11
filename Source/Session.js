da537bd65bb7efd3bc42310211e350cec37bba6e
thiscouldbebetter <thiscouldbebetter@users.noreply.github.com>
2019-10-10 21:25:09 -0400

    Added pane containing line offsets.  Modified ASCII pane to follow the current scroll position of the hexadecimal pane.

--- Source/Session.js
+++ Source/Session.js
@@ -1,4 +1,4 @@
- 
+
 function Session(bytes)
 {
 	this.bytes = bytes;
@@ -6,104 +6,149 @@ function Session(bytes)
 }
 {
 	// dom
- 
+
 	Session.prototype.domElementUpdate = function()
 	{
+		var nibblesPerRow = 32;
+		var bytesPerRow = nibblesPerRow / 2;
+
 		if (this.domElement == null)
 		{
-			var divSession = document.createElement("div");
- 
-			var textareaHexadecimal = document.createElement("textarea");
-			textareaHexadecimal.cols = 40;
-			textareaHexadecimal.rows = 10;
+			var d = document;
+
+			var divSession = d.createElement("div");
+
+			var rowCount = 32;
+
+			var textareaOffsets = d.createElement("textarea")
+			textareaOffsets.cols = 8;
+			textareaOffsets.rows = rowCount;
+			textareaOffsets.disabled = true;
+			textareaOffsets.style.resize = "none";
+			textareaOffsets.spellcheck = false;
+			this.textareaOffsets = textareaOffsets;
+			divSession.appendChild(textareaOffsets);
+
+			var textareaHexadecimal = d.createElement("textarea");
+			textareaHexadecimal.rows = rowCount;
 			textareaHexadecimal.onkeyup = this.textareaHexadecimal_KeyUp.bind(this);
 			textareaHexadecimal.oninput = this.textareaHexadecimal_Changed.bind(this);
+			textareaHexadecimal.style.resize = "none";
+			textareaHexadecimal.spellcheck = false;
 			this.textareaHexadecimal = textareaHexadecimal;
 			divSession.appendChild(textareaHexadecimal);
- 
-			var textareaASCII = document.createElement("textarea");
-			textareaASCII.cols = 20;
-			textareaASCII.rows = 10;
+
+			var textareaASCII = d.createElement("textarea");
+			textareaASCII.cols = bytesPerRow - 1; // Not sure why -1 is needed.
+			textareaASCII.rows = rowCount;
 			textareaASCII.disabled = true;
+			textareaASCII.style.resize = "none";
+			textareaASCII.spellcheck = false;
 			this.textareaASCII = textareaASCII;
 			divSession.appendChild(textareaASCII);
- 
-			var divFileOperations = document.createElement("div");
-		 
-			var buttonSave = document.createElement("button");
+
+			var divFileOperations = d.createElement("div");
+
+			var buttonSave = d.createElement("button");
 			buttonSave.innerHTML = "Save";
 			buttonSave.onclick = this.buttonSave_Clicked.bind(this);
 			divFileOperations.appendChild(buttonSave);
- 
-			var inputFileToLoad = document.createElement("input");
+
+			var inputFileToLoad = d.createElement("input");
 			inputFileToLoad.type = "file";
 			inputFileToLoad.onchange = this.inputFileToLoad_Changed.bind(this);
 			divFileOperations.appendChild(inputFileToLoad);
- 
+
 			divSession.appendChild(divFileOperations);
- 
-			var divCursor = document.createElement("div");
-			 
-			var labelCursorPosition = document.createElement("label");
+
+			var divCursor = d.createElement("div");
+
+			var labelCursorPosition = d.createElement("label");
 			labelCursorPosition.innerHTML = "Cursor Position:";
 			divCursor.appendChild(labelCursorPosition);
- 
-			var inputCursorPosition = document.createElement("input");
+
+			var inputCursorPosition = d.createElement("input");
 			inputCursorPosition.disabled = true;
 			this.inputCursorPosition = inputCursorPosition;
 			divCursor.appendChild(inputCursorPosition);
- 
+
 			divSession.appendChild(divCursor);
- 
-			var divMain = document.getElementById("divMain");
+
+			var divMain = d.getElementById("divMain");
 			divMain.appendChild(divSession);
- 
+
 			this.domElement = divSession;
 		}
- 
+
+		var textareaHexadecimalWidthInColumns = nibblesPerRow - 1; // Not sure why -1 is needed.
+		if (this.textareaHexadecimal.scrollHeight > this.textareaHexadecimal.clientHeight)
+		{
+			var scrollbarWidthInChars = 2;  // May be 3 on some systems?
+			textareaHexadecimalWidthInColumns += scrollbarWidthInChars;
+		}
+		this.textareaHexadecimal.cols = textareaHexadecimalWidthInColumns;
+
 		var bytesAsStringHexadecimal = Converter.bytesToStringHexadecimal
 		(
 			this.bytes
 		);
-		this.textareaHexadecimal.value = 
+		this.textareaHexadecimal.value =
 			bytesAsStringHexadecimal + this.finalNibble;
- 
-		var bytesAsStringASCII = Converter.bytesToStringASCII
-		(
-			this.bytes
-		);
-		this.textareaASCII.value = bytesAsStringASCII;
- 
-		var cursorPos = this.textareaHexadecimal.selectionStart;
-		var cursorPosAsString = 
-			"0d" + cursorPos 
-			+ "; 0x" + cursorPos.toString(16)
+
+		var charsPerByte = 2;
+		var cursorPos = Math.floor(this.textareaHexadecimal.selectionStart / charsPerByte);
+		var cursorPosAsString =
+			"0x" + cursorPos.toString(16)
+			+ "; 0d" + cursorPos
 			+ "; 0b" + cursorPos.toString(2);
- 
+
 		this.inputCursorPosition.value = cursorPosAsString;
- 
+
+		var rowsVisible = this.textareaHexadecimal.rows;
+		var rowHeightInPixels = this.textareaHexadecimal.offsetHeight / rowsVisible;
+		var scrollOffsetInPixels = this.textareaHexadecimal.scrollTop;
+		var scrollOffsetInRows = Math.round(scrollOffsetInPixels / rowHeightInPixels);
+		var scrollOffsetInBytes = scrollOffsetInRows * bytesPerRow;
+		var offsetsAsStrings = [];
+		var bytesForRowsAsASCII = [];
+		for (var i = 0; i < rowsVisible; i++)
+		{
+			var offsetForRow = scrollOffsetInBytes + (i * bytesPerRow);
+			var offsetForRowAsHexadecimal = offsetForRow.toString(16)
+			offsetsAsStrings.push(offsetForRowAsHexadecimal);
+
+			var bytesForRow = this.bytes.slice(offsetForRow, offsetForRow + bytesPerRow);
+			var bytesForRowAsASCII = Converter.bytesToStringASCII(bytesForRow)
+			bytesForRowsAsASCII.push(bytesForRowAsASCII);
+		}
+		var offsetsAsString = offsetsAsStrings.join("\n");
+		this.textareaOffsets.value = offsetsAsString;
+
+		var bytesAsStringASCII = bytesForRowsAsASCII.join("\n");
+		this.textareaASCII.value = bytesAsStringASCII;
+
 		return this.domElement;
 	}
- 
+
 	// events
- 
+
 	Session.prototype.buttonSave_Clicked = function()
 	{
 		var dataAsArrayBuffer = new ArrayBuffer(this.bytes.length);
 		var dataAsArrayUnsigned = new Uint8Array(dataAsArrayBuffer);
-		for (var i = 0; i < this.bytes.length; i++) 
+		for (var i = 0; i < this.bytes.length; i++)
 		{
 			dataAsArrayUnsigned[i] = this.bytes[i];
 		}
 		var dataAsBlob = new Blob([dataAsArrayBuffer], {type:'bytes'});
- 
- 
+
+
 		var link = document.createElement("a");
 		link.href = window.URL.createObjectURL(dataAsBlob);
 		link.download = "Data.bin";
 		link.click();
 	}
- 
+
 	Session.prototype.inputFileToLoad_Changed = function(event)
 	{
 		var inputFileToLoad = event.target;
@@ -111,26 +156,26 @@ function Session(bytes)
 		if (fileToLoad != null)
 		{
 			var fileReader = new FileReader();
-			fileReader.onload = this.inputFileToLoad_Changed_Loaded.bind(this); 
+			fileReader.onload = this.inputFileToLoad_Changed_Loaded.bind(this);
 			fileReader.readAsBinaryString(fileToLoad);
 		}
 	}
- 
-	Session.prototype.inputFileToLoad_Changed_Loaded = function(fileLoadedEvent) 
+
+	Session.prototype.inputFileToLoad_Changed_Loaded = function(fileLoadedEvent)
 	{
 		var dataAsBinaryString = fileLoadedEvent.target.result;
- 
+
 		this.bytes = [];
- 
+
 		for (var i = 0; i < dataAsBinaryString.length; i++)
 		{
 			var byte = dataAsBinaryString.charCodeAt(i);
 			this.bytes.push(byte);
 		}
- 
+
 		this.domElementUpdate();
 	}
- 
+
 	Session.prototype.textareaHexadecimal_Changed = function(event)
 	{
 		var bytesAsStringHexadecimal = event.target.value;
@@ -138,7 +183,7 @@ function Session(bytes)
 		(
 			bytesAsStringHexadecimal
 		);
- 
+
 		if (bytesAsStringHexadecimal.length % 2 == 0)
 		{
 			this.finalNibble = "";
@@ -150,23 +195,24 @@ function Session(bytes)
 				bytesAsStringHexadecimal.length - 1,
 				1
 			);
- 
+
 			var finalNibbleAsInt = parseInt(this.finalNibble, 16);
 			if (isNaN(finalNibbleAsInt) == true)
 			{
 				this.finalNibble = "";
 			}
 		}
- 
+
 		this.domElementUpdate();
 	}
- 
+
 	Session.prototype.textareaHexadecimal_KeyUp = function(event)
 	{
-		if (event.key.startsWith("Arrow") == true)
+		var keyName = event.key;
+		if (keyName.startsWith("Arrow") || keyName == "Home" || keyName == "End")
 		{
 			this.domElementUpdate();
 		}
 	}
- 
+
 }
